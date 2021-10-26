09237ed951f18016d260a247ac6d1132b2f01687
Anmol Jain <defini>
2021-10-26 23:04:41 +0530

    UPDATE: Minor Changes in Country Model

--- WhatsAppMe/Country.swift
+++ WhatsAppMe/Country.swift
@@ -7,10 +7,19 @@
 
 import Foundation
 
-struct Country: Decodable, Identifiable {
+struct Country: Identifiable {
 	let id: UUID = UUID()
 	let code: String
 	let flag: String
 	let alpha2: String
 	let name: String
+}
+
+extension Country: Decodable {
+	enum CodingKeys: String, CodingKey {
+		case code
+		case flag
+		case alpha2
+		case name
+	}
 }
